831fb03a44a595ea2d6bd0b8cabae7e60f63283c
lama saad <69835476+LamaSaad24@users.noreply.github.com>
2024-10-18 07:41:04 +0300

    Update TourController.php

--- app/Http/Controllers/Api/TourController.php
+++ app/Http/Controllers/Api/TourController.php
@@ -107,7 +107,7 @@ class TourController extends Controller
             'price' => 'required|numeric|between:0,9999.99',
             'startDate' => 'required|date_format:Y-m-d',
             'endDate' => 'required|date_format:Y-m-d',
-            'description' => 'required|max:255',
+            'description' => 'required|min:15',
         ],[
             //messages for Errores...............................
 
